1946b50d7e68e64f9c75f5818ae04b02e47a2b8b
SujonHossain1 <sujon.hossain758@gmail.com>
2020-08-17 16:57:38 +0600

    booked added

--- js/app.js
+++ js/app.js
@@ -1,33 +1,53 @@
+
 const ul = document.querySelector("#book-list ul");
-        ul.addEventListener('click', (event) =>{
-           if(event.target.className == "delete"){
-               const li = event.target.parentElement;
-               ul.removeChild(li);
-           }
-        })
 
-        const form = document.forms['add-new-book'];
-        form.addEventListener('submit', function(event) {
-            const book = event.target.querySelector("#book").value.trim();
-            createBookListItem(book);
+ul.addEventListener('click', (event) => {
+    if (event.target.className == "delete") {
+        const li = event.target.parentElement;
+        ul.removeChild(li);
+    }
+})
 
-            event.target.querySelector("#book").value = "";
-            event.preventDefault();
-        })
+const form = document.forms['add-new-book'];
+form.addEventListener('submit', function (event) {
+    const book = event.target.querySelector("#book").value.trim();
+    
+    if(book.length <= 0){
+        // alert("Empty field not allow!");
+        error();
+    }else{
+        createBookListItem(book);
+    }
 
-        
-         // create element 
-         function createBookListItem(value) {
-            const li = document.createElement('li');
-            const bookName = document.createElement('span');
-            const deleteBtn = document.createElement('span');
+    event.target.querySelector("#book").value = "";
+    event.preventDefault();
+})
 
-            bookName.innerText = value;
-            bookName.classList.add('name');
-            deleteBtn.classList.add('delete');
-            deleteBtn.innerText = "delete";
-            
-            li.appendChild(bookName);
-            li.appendChild(deleteBtn);
-            ul.appendChild(li);
-        }
+
+// create element 
+function createBookListItem(value) {
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    bookName.innerText = value;
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerText = "delete";
+
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+}
+
+// error function
+
+function error(){
+    form.innerHTML += `
+    <span style="color: red">Empty field not allow! </span>
+    `;
+    setTimeout(() =>{
+        document.querySelector("#add-new-book span").style.display = "none";
+    }, 2000);
+   
+};
